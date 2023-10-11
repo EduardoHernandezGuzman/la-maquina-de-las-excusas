dd537c064f4788d3f4486f9ed6aa6d038ca62972
EduardoHG <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-10-11 12:14:38 +0000

    creación y modificación de .js .html y .css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "Pennywise",
+    "El Jinete sin Cabeza",
+    "Un selenita",
+    "Un ser venido de otro planeta",
+    "Godzilla",
+    "Drácula",
+    "Cthulhu"
+  ];
+  let action = [
+    "comió",
+    "destruyó",
+    "quemó",
+    "desintegró",
+    "pulverizó",
+    "escondió",
+    "partió"
+  ];
+  let what = [
+    "las llaves de mi casa",
+    "el tejado del vecino",
+    "mi coche nuevo",
+    "las gafas de mi abuela",
+    "los apuntes de HTML",
+    "el monopatín de mi hermano pequeño"
+  ];
+  let when = [
+    "después del almuerzo",
+    "justo a tiempo",
+    "después del concierto",
+    "durante el viaje de fin de curso",
+    "depués de las clases",
+    "durante la misa del domingo",
+    "durante la proyección de la película"
+  ];
+
+  function createExcuse() {
+    let whoRamdom = who[Math.floor(Math.random() * who.length)];
+    let actionRamdom = action[Math.floor(Math.random() * action.length)];
+    let whatRamdom = what[Math.floor(Math.random() * what.length)];
+    let whenRamdom = when[Math.floor(Math.random() * when.length)];
+
+    return `${whoRamdom} ${actionRamdom} ${whatRamdom} ${whenRamdom}`;
+  }
+
+  const excuseElement = document.getElementById("excuse");
+  const generatedExcuse = createExcuse();
+  excuseElement.textContent = generatedExcuse;
 };
+
+function refreshPage() {
+  location.reload();
+}
